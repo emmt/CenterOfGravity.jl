4b84c0a03c7737b1ada3760dd167dea8b034cc66
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2022-05-11 09:12:09 +0200

    Improve code coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,32 @@ using Test, Random
 @testset "Windowed arrays  " begin
     A = reshape(collect(1:20), (4,5))
     J = (2:3, 3:5)
+    j_first = map(first, J)
+    j_last = map(last, J)
     B = WindowedArray(A, J...)
+    @test IndexStyle(B) === IndexCartesian()
     @test eltype(B) === eltype(A)
     @test parent(B) === A
     @test axes(B) == J
     @test size(B) == map(length, J)
     @test [B[j1,j2] for j1 in J[1], j2 in J[2]] == [A[j1,j2] for j1 in J[1], j2 in J[2]]
+    v_first = A[j_first...]
+    v_last = A[j_last...]
+    B[j_first...] = v_last
+    @test A[j_first...] == v_last
+    B[j_last...] = v_first
+    @test A[j_last...] == v_first
+
+    # Check range conversion and full range specified by a colon.
+    B = WindowedArray(A, Int16(2):Int16(3), :)
+    @test IndexStyle(B) === IndexCartesian()
+    @test eltype(B) === eltype(A)
+    @test parent(B) === A
+    J = axes(B)
+    @test J == (2:3, axes(A,2))
+    @test size(B) == map(length, J)
+    @test [B[j1,j2] for j1 in J[1], j2 in J[2]] == [A[j1,j2] for j1 in J[1], j2 in J[2]]
+
 end
 
 @testset "Sliding windows  " begin
@@ -17,11 +37,15 @@ end
     A = reshape(collect(1:20), (4,5))
     B = SlidingWindow(A)
     J = map(n -> -div(n,2) : n-1-div(n,2), size(A))
+    j = map(last, J)
     @test eltype(B) === eltype(A)
     @test parent(B) === A
     @test axes(B) == J
     @test size(B) == size(A)
     @test [B[j1,j2] for j1 in J[1], j2 in J[2]] == A
+    val = B[j...]
+    B[j...] = -val
+    @test A[end,end] == -val
 
     # Sliding window with given axes.
     J = map(n -> Int16(0) : Int16(n-1), size(A))
@@ -33,13 +57,16 @@ end
     @test [B[j1,j2] for j1 in J[1], j2 in J[2]] == A
 
     # Uniformly true mask.
-    J = (-1:7, 0:4, 3:8)
+    J = (-1:7, Int16(0):Int16(4), 3:8)
+    j = map(first, J)
     B = SlidingWindow(J...)
     @test eltype(B) === Bool
     @test parent(B) === CenterOfGravity.SlidingWindows.UniformlyTrue()
     @test axes(B) == J
     @test size(B) == map(length, J)
     @test [B[j1,j2,j3] for j1 in J[1], j2 in J[2], j3 in J[3]] == ones(Bool, size(B))
+    @test B[j...] == true
+    @test_throws ErrorException B[j...] = false
 end
 
 distance(a::NTuple{N,Real}, b::NTuple{N,Real}) where {N} =
